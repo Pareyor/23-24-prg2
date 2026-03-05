4a6f6945291371458d7a95d04e82aa1138a7cdcb
Pareyor <144883777+Pareyor@users.noreply.github.com>
2026-03-05 14:04:19 +0100

    correccion formato en clase Visitante

--- entregas/reyPablo/Reto-003/Visitante.java
+++ entregas/reyPablo/Reto-003/Visitante.java
@@ -1,47 +1,35 @@
+public class Visitante {
+    private String nombre;
+    private String apellido;
+    private int numeroDeIdentidad;
 
-    public class Visitante {
-        private String nombre;
-        private String apellido;
-        private int numeroDeIndentidad;
-        
-        
-        public Visitante(String nombre, String apellido, int numeroDeIndentidad) {
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.numeroDeIndentidad = numeroDeIndentidad;
-        }
-    
-    
-        public String getNombre() {
-            return nombre;
-        }
-    
-    
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-    
-    
-        public String getApellido() {
-            return apellido;
-        }
-    
-    
-        public void setApellido(String apellido) {
-            this.apellido = apellido;
-        }
-    
-    
-        public int getNumeroDeIndentidad() {
-            return numeroDeIndentidad;
-        }
-    
-    
-        public void setNumeroDeIndentidad(int numeroDeIndentidad) {
-            this.numeroDeIndentidad = numeroDeIndentidad;
-        }
-        
-        
-       
-    
+    public Visitante(String nombre, String apellido, int numeroDeIdentidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDeIdentidad = numeroDeIdentidad;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroDeIdentidad() {
+        return numeroDeIdentidad;
+    }
+
+    public void setNumeroDeIdentidad(int numeroDeIdentidad) {
+        this.numeroDeIdentidad = numeroDeIdentidad;
+    }
+}
